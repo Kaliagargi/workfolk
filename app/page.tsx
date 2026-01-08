eadda0a62fc64fbd84a15dadceb6104ea432c482
kaliagargi <gargikalia18@gmail.com>
2026-01-08 18:03:48 +0530

    to be removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const MapClient = dynamic(
-  () => import("../components/MapClient"),
-  { ssr: false }
-);
+import MapWrapper from "@/components/mapwrapper";
 
 export default function Home() {
   return (
-    <main style={{ padding: "20px" }}>
-      <h1>NDWI Viewer</h1>
-      <p>Click on the map to compute NDWI</p>
-      <MapClient />
+    <main style={{ height: "100vh", width: "100%" }}>
+      <MapWrapper />
     </main>
   );
 }
