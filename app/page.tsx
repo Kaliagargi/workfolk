309eaf1a2dc12732a5a05ccc8762f64c18c80848
kaliagargi <gargikalia18@gmail.com>
2026-01-08 17:51:40 +0530

    will go back

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,8 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const MapClient = dynamic(
-  () => import("../components/MapClient"),
-  { ssr: false }
-);
-
 export default function Home() {
   return (
-    <main style={{ padding: "20px" }}>
-      <h1>NDWI Viewer</h1>
-      <p>Click on the map to compute NDWI</p>
-      <MapClient />
+    <main style={{ padding: 40 }}>
+      <h1>Vercel is alive ✅</h1>
+      <p>If you see this, routing works.</p>
     </main>
   );
 }
